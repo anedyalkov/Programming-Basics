b1443c9c888bf55c7fed1a6aee5846fbbb0059d1
anedyalkov <nedyalkov78@gmail.com>
2017-05-25 21:13:45 +0300

    Simple Loops

---
+++ Simple Loops/Left and Right Sum/Left and Right Sum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sum = 0;
+
+            for (var i = 1; i <= n; i++)
+            {
+
+
+                var num = int.Parse(Console.ReadLine());
+                sum = sum + num;
+            }
+            //Console.WriteLine(sum);
+
+            var sum1 = 0;
+            for (var i = 1; i <= n; i++)
+            {
+
+
+                var num = int.Parse(Console.ReadLine());
+                sum1 = sum1 + num;
+            }
+            //Console.WriteLine(sum1);
+            if (sum == sum1)
+            {
+                Console.WriteLine(" Yes, sum = " + Math.Abs(sum));
+            }
+            if (sum != sum1)
+            {
+                Console.WriteLine(" No, diff = " + Math.Abs(sum - sum1));
+            }
+
+        }
+    }
+}
---
+++ Simple Loops/Odd Even Sum/Odd Even Sum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var evenSum = 0;
+            var oddSum = 0;
+
+
+            for (var i = 0; i <= n - 1; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    evenSum = evenSum + number;
+                }
+                else
+
+                    oddSum = oddSum + number;
+            }
+
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + oddSum);
+            }
+
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("diff = " + Math.Abs(oddSum - evenSum));
+            }
+        }
+    }
+}
