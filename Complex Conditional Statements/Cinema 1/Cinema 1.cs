570359c9904db1637010f7ddd9deb61933829341
anedyalkov <nedyalkov78@gmail.com>
2017-07-13 23:24:16 +0300

    Complex Conditional Statements

---
+++ Complex Conditional Statements/Cinema 1/Cinema 1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movie = Console.ReadLine().ToLower();
+            double RowNumbers = double.Parse(Console.ReadLine());
+            double ColumnNumbers = double.Parse(Console.ReadLine());
+
+            var result = 0.0;
+
+            if (movie == "premiere")
+            {
+                result = 12.00;
+            }
+            else if (movie == "normal")
+            {
+                result = 7.50;
+            }
+            else if (movie == "discount")
+            {
+                result = 5.00;
+            }
+
+            // if (result > 0) ;
+            Console.WriteLine("{0:f2}", result * RowNumbers * ColumnNumbers);
+        }
+    }
+}
---
+++ Complex Conditional Statements/Cinema/Cinema.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema
+{
+    class Cinema
+    {
+        static void Main(string[] args)
+        {
+            string movie = Console.ReadLine().ToLower();
+            double RowNumbers = double.Parse(Console.ReadLine());
+            double ColumnNumbers = double.Parse(Console.ReadLine());
+            var sum = RowNumbers * ColumnNumbers;
+            double result = 0.0;
+
+            if (movie == "premiere")
+            {
+                result = sum * 12.00;
+            }
+            else if (movie == "normal")
+            {
+                result = sum * 7.50;
+            }
+            else if (movie == "discount")
+            {
+                result = sum * 5.00;
+            }
+
+            //if (result > 0) ;
+            Console.WriteLine("{0:f2}", result)
+        }
+    }
+}
